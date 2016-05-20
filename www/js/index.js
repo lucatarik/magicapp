dd64917e84b6b1a4b0b1fd9dbaa656e50db8d239
a.namici <a.namici@OptiPlex-360-Andrea>
2016-05-20 14:45:51 +0200

    completed simple functions

--- www/js/index.js
+++ www/js/index.js
@@ -40,12 +40,39 @@ var app = {
         });
 
         bindEvents();
-
+        loadDefault();
 
     }
 };
 
 app.initialize();
+
+function loadDefault()
+{
+    var listview = $('ul[data-role="listview"][data-alphabet="true"]');
+
+    cdb.executeSql("SELECT `_rowid_` id,nome,livello from magie;", [], function (res) {
+        for (var i = 0; i < res.rows.length; i++) {
+            var row = res.rows.item(i);
+            if (row.livello == 0)
+                row.livello = "Trucchetto";
+            listview.append(TemplateEngine($('#listviewrow').html(), row));
+        }
+        listview.alphabetlist();
+    }, function (error) {
+        alert('SELECT error: ' + error.message);
+    });
+    var classeslist = $('#classeslist');
+    cdb.executeSql("select distinct classe from categorie;", [], function (res) {
+        for (var i = 0; i < res.rows.length; i++) {
+            var row = res.rows.item(i);
+            classeslist.append(TemplateEngine($('#classesHTML').html(), row));
+        }
+        listview.alphabetlist();
+    }, function (error) {
+        alert('SELECT error: ' + error.message);
+    });    
+}
 
 
 function WriteResult(result)
@@ -113,19 +140,40 @@ function dresultStr(data)
 
 function bindEvents()
 {
-    $.event.special.swipe.scrollSupressionThreshold = 10; // More than this horizontal displacement, and we will suppress scrolling.
-    $.event.special.swipe.horizontalDistanceThreshold = 30; // Swipe horizontal displacement must be more than this.
-    $.event.special.swipe.durationThreshold = 500;  // More time than this, and it isn't a swipe.
-    $.event.special.swipe.verticalDistanceThreshold = 75; // Swipe vertical displacement must be less than this.
-    $('ul[data-role="listview"][data-alphabet="true"]').alphabetlist();
+    /*$.event.special.swipe.scrollSupressionThreshold = 10; // More than this horizontal displacement, and we will suppress scrolling.
+     $.event.special.swipe.horizontalDistanceThreshold = 30; // Swipe horizontal displacement must be more than this.
+     $.event.special.swipe.durationThreshold = 500;  // More time than this, and it isn't a swipe.
+     $.event.special.swipe.verticalDistanceThreshold = 75; // Swipe vertical displacement must be less than this.
+     $('div[data-role="page"]').on("swiperight", function (e)
+     {
+     //if ($(e.target).is('input'))
+     return;
+     var elems = $('[role="tab"].ui-state-active').prevAll();
+     if (elems.length)
+     {
+     elems.eq(0).find('a').click();
+     }
+     }).on("swipeleft", function (e)
+     {
+     //if ($(e.target).is('input'))
+     return;
+     var elems = $('[role="tab"].ui-state-active').nextAll();
+     if (elems.length)
+     {
+     elems.eq(0).find('a').click();
+     }
+     });*/
 
-        // these events affect a listview but are outside its control:
-        // when scrolling or changing pages, redraw any visible alphabetlists
-        $(document).bind('scrollstop pagechange', function () {
-            var lists = $.mobile.activePage.find('ul[data-role="listview"][data-alphabet="true"]');
-            if (lists.length) lists.alphabetlist('redraw');
-        });
-        
+    $('#clear').bind("click", function () {
+        if (confirm("Sure?"))
+            $('.content').html('')
+    });
+
+    $('#diceroller').bind("click", function ()
+    {
+        WriteResult(dresultStr(roll($('#dicerollerval').val())));
+    });
+
     $('.getrand').bind('click', function ()
     {
         var table = $(this).attr('id');
@@ -135,16 +183,41 @@ function bindEvents()
             alert('SELECT error: ' + error.message);
         });
     });
-    
-    $('#cards').bind('click',function()
+
+    $('#cards').bind('click', function ()
     {
-       cdb.executeSql("SELECT card as field from card WHERE _ROWID_ >= (abs(random()) % (SELECT max(_ROWID_) FROM card)) LIMIT 9;", [], function (res) {            
-            $('#popupLogin img').each(function(i)
+        cdb.executeSql("SELECT card as field from card WHERE _ROWID_ >= (abs(random()) % (SELECT max(_ROWID_) FROM card)) LIMIT 9;", [], function (res) {
+            $('#popupLogin img').each(function (i)
             {
-                $(this).attr('src','img/magicimages/'+res.rows.item(i).field);
+                $(this).attr('src', 'img/magicimages/' + res.rows.item(i).field);
             })
         }, function (error) {
             alert('SELECT error: ' + error.message);
-        }); 
+        });
     });
+
+    // these events affect a listview but are outside its control:
+    // when scrolling or changing pages, redraw any visible alphabetlists
+    $(document).bind('scrollstop pagechange', function () {
+        var lists = $.mobile.activePage.find('ul[data-role="listview"][data-alphabet="true"]');
+        if (lists.length)
+            lists.alphabetlist('redraw');
+    });
+    $('#mainlistview').on("click",".listviewrow",function()
+    {
+        var id = $(this).attr('data-id');
+        cdb.executeSql("SELECT m.`_rowid_` id,m.*,group_concat(c.classe) as classe  \
+                            FROM `magie` m left join `categorie` c on c.nome = m.nome  \
+                            where id = ? group by m.nome ORDER BY `nome` ASC ", [id], function (res) 
+        {
+            if (res.rows.length)
+            {
+                var row = res.rows.item(0);
+                $('#popupDialog').html(TemplateEngine($('#popupHTML').html(), row)).popup('open');
+            }
+        }, function (error) {
+            alert('SELECT error: ' + error.message);
+        });
+    });
+
 }
